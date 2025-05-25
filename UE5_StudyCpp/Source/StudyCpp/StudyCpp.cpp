4e3a4b4cdee3b15cbec5af768f67e35cca41aacd
threedowon <3dowon@gmail.com>
2025-05-25 14:59:30 +0900

    Init

---
+++ UE5_StudyCpp/Source/StudyCpp/StudyCpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StudyCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StudyCpp, "StudyCpp" );
